acb555809c29818f166e4dae86d2f1061f059312
高見知英 <chie@onpu-tamago.net>
2016-03-05 16:49:38 +0900

    1.0対応

--- imagecurdir.php
+++ imagecurdir.php
@@ -7,35 +7,38 @@
  * @license http://opensource.org/licenses/MIT
  * @version 1.0
  */
-class ImageCurDir{
+class ImageCurDir extends AbstractPicoPlugin{
 
   private $base_url;
   
-  public function config_loaded(&$settings) {
-    $this->base_url = $settings['base_url'];
-    $this->content_dir = $settings['content_dir'];
+  public function onConfigLoaded(array &$config) {
+    $this->base_url = $config['base_url'];
+    $this->content_dir = $config['content_dir'];
   }
 
-  public function before_read_file_meta(&$headers)
+  public function onMetaHeaders(array &$headers)
   {
   	$headers['image'] = 'Image';
   }
 	
-	public function get_page_data(&$data, $page_meta)
-	{
-    $file_url = substr($data["url"], strlen($this->base_url));
+	public function onSinglePageLoaded(array &$pageData)
+  {
+    $file_url = substr($pageData["url"], strlen($this->base_url));
     if($file_url[strlen($file_url) - 1] == "/") $file_url .= 'index';
-    if (strlen($page_meta['image']) > 0 && preg_match('/^(.*\/)[\w\.-]+$/', $file_url, $m)) {
-      if($page_meta['image'][0] == '.'){
-        $data['image'] = $this->base_url . "/" . $this->content_dir . "$m[0]$page_meta[image]";
-      }else if(!preg_match('/^(https?|ftp)/', $page_meta['image'])){
-        $data['image'] = $this->base_url . "/" . $this->content_dir . "$m[1]$page_meta[image]";
-      }else{
-        $data['image'] = $page_meta['image'];
+    $img = $pageData['meta']['image'];
+    // rootdirを除去する(1.0対応)
+    $rootdir = $this->getPico()->getRootDir();
+    $dir = substr($this->content_dir, strlen($rootdir));
+    if (strlen($img) > 0 && preg_match('/^(.*\/)[\w\.-]+$/', $file_url, $m)) {
+      if($img[0] == '.'){
+        $img = $this->base_url . "/$dir$m[0]$img";
+      }else if(!preg_match('/^(https?|ftp)/', $img)){
+        $img = $this->base_url . "/$dir$m[1]$img";
       }
     } else {
-      $data['image'] = NULL;
+      $img = NULL;
     }
+    $pageData['image'] = $img;
 	}
 }
 
