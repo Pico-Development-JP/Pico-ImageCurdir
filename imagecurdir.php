f0d30d4182216fa7ebf0d98d0e0c480fb08a7626
高見知英 <chie@onpu-tamago.net>
2016-03-15 23:56:33 +0900

    バージョン変更

--- imagecurdir.php
+++ imagecurdir.php
@@ -5,7 +5,7 @@
  * @author TakamiChie
  * @link http://onpu-tamago.net/
  * @license http://opensource.org/licenses/MIT
- * @version 1.0
+ * @version 1.1
  */
 class ImageCurDir extends AbstractPicoPlugin{
 
